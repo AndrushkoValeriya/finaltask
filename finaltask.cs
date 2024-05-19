74108c9e3a2cfe9004f5df6ec0203cb81a87de05
Valeriya <valeri.andrushko@yandex.ru>
2024-05-19 16:44:26 +0300

    Создание метода введения элементов массива

--- finaltask.cs
+++ finaltask.cs
@@ -1,3 +1,14 @@
-Console.Clear;
-Console.Write("Введите количество элементов массива:");
-int n = Console.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string [] Array = new string [n];
+void array(string [] Array)
+{
+  for (int i = 0; i < Array.Length ; i++)
+  {
+     Console.WriteLine($"Введите {i+1} элемент массива");
+     Array[i] = Console.ReadLine();
+  }
+}
+
